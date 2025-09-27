28810c2d07abcfb17f46d2c1ece82e3fce14eaa5
road2qnt <egaluthfi.el@gmail.com>
2025-09-27 08:32:50 +0700

    feat: complete boot and GDT setup

--- src/kernel.c
+++ src/kernel.c
@@ -4,9 +4,7 @@
 #include "header/stdlib/boolean.h"
 
 void kernel_setup(void) {
-    uint32_t a;
-    uint32_t volatile b = 0x0000BABE;
-    __asm__("mov $0xCAFE0000, %0" : "=r"(a));
-    while (true) b += 1;
+    load_gdt(&_gdt_gdtr);
+    while (true);
 }
 
