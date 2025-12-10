df5b2cb269f9318a44ba124924646a1891eb7df6
leains <leonardusbrain0@gmail.com>
2025-12-10 08:21:57 +0700

    fix: minor bug fix

--- src/header/stdlib/boolean.h
+++ src/header/stdlib/boolean.h
@@ -1,7 +1,12 @@
 #ifndef BOOLEAN_H
 #define BOOLEAN_H
 
-
-#define true 1
+// Boolean values (lowercase - C99 style)
+#define true  1
 #define false 0
+
+// Boolean values (uppercase - legacy compatibility)
+#define TRUE  1
+#define FALSE 0
+
 #endif
